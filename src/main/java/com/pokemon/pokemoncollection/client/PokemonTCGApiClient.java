b5df85f1a3fc26dc1ed0035b7ba5f99c433b7178
Paula Sochaczyńska <paula.weronika.es@gmail.com>
2020-05-19 19:18:09 +0200

    Add Api Client class and connect this class object to pack opener.

---
+++ src/main/java/com/pokemon/pokemoncollection/client/PokemonTCGApiClient.java
@@ -0,0 +1,13 @@
+package com.pokemon.pokemoncollection.client;
+
+import com.pokemon.pokemoncollection.model.Card;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class PokemonTCGApiClient {
+    public List<Card> downloadCards(){
+
+    }
+}
